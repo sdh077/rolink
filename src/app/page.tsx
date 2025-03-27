38ff2649f0729a6518012eb000a483a9b5b81a82
Donggyu-Lee <leedg84@gmail.com>
2025-03-28 00:53:09 +0900

    design update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,9 +64,22 @@ const Church = async ({ username }: { username: string | undefined }) => {
     pre[`${apply.year}-${apply.month}`] = apply;
     return pre;
   }, {} as ApplyObj) ?? {}
+  const theadClass='py-3 px-4 text-center text-gray-500 text-normal'
+  const tbodyClass='py-4 px-4 text-center text-gray-800 text-normal'
+  const currentMonth=3
+  const sampleDonation= [
+    {
+      cafe: '파브스',
+      value: 3
+    },
+    {
+      cafe: '링크트',
+      value: 4
+    }
+  ] 
 
   return (
-    <div className='bg-slate-100 min-h-screen'>
+    <div className='bg-slate-100 min-h-screen h-full'>
       {/* top nav bar design */}
       <div className='h-[70px] bg-yellow-900 flex'>
         <div className='container mx-auto flex items-center justify-center'>
@@ -83,8 +96,35 @@ const Church = async ({ username }: { username: string | undefined }) => {
         </div>
       </div>
 
-      <div className="container mx-auto p-5">
-        <div className='grid grid-cols-2 gap-4 '>
+    <div className='py-10'>
+    <div className="container mx-auto p-5 bg-white rounded-xl">
+        <table className='w-full'>
+          <thead className='border-b'>
+            <tr>
+              <td className={theadClass}>월</td>
+              <td className={theadClass}>원두 신청/수정</td>
+              <td className={theadClass}>신청 중량 (kg)</td>
+              <td className={theadClass}>달성률</td>
+              <td className={theadClass}>기부 카페</td>
+            </tr>
+          </thead>
+          <tbody>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(month => 
+            <tr className={ month < currentMonth ? 'bg-gray-300' : 'hover:bg-yellow-100' }>
+              <td className={tbodyClass+' w-32'}>{month}월</td>
+              <td className={tbodyClass+' w-60'}><AddApply year={2025} month={month} apply={obj[`${2025}-${month}`]} /></td>
+              <td className={tbodyClass+' w-40'}>{obj[`${2025}-${month}`]?.bean}</td>
+              <td className={tbodyClass+' w-40'}>-</td>
+              <td className={tbodyClass+' flex gap-5 items-center'}>
+                {sampleDonation.map(e => 
+                  <div className="bg-yellow-900 rounded-xl px-2.5 py-1 tracking-tight text-normal text-white">{e.cafe} {e.value}kg</div>
+                )}
+              </td>
+            </tr>
+          )}
+          </tbody>
+        </table>
+        {/* <div className='grid grid-cols-2 gap-4 '>
           {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(month =>
             <div key={month} className='flex gap-4'>
               <div className='flex flex-col justify-center text-center w-64 h-32 border-2'>
@@ -97,8 +137,10 @@ const Church = async ({ username }: { username: string | undefined }) => {
               <div>링크트 3키로</div>
             </div>
           )}
-        </div>
+        </div> */}
       </div>
+    </div>
+     
 
     </div>
   )
