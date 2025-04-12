c55d594449a121c650c618a9c1c62f8806aa2b07
shindaeho <sdh077@naver.com>
2025-04-12 16:59:39 +0900

    변경

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button'
 import { login } from './actions'
 
 export default function LoginPage() {
@@ -5,7 +6,6 @@ export default function LoginPage() {
     <form className='flex justify-center items-center w-full h-screen bg-slate-200'>
       <div className="w-[500px] h-[500px] border rounded-2xl py-8 px-12 bg-white flex items-center justify-center">
         <div className='w-full'>
-          {/* title 넣거나 이미지 넣으셈 */}
           <div className='text-center text-3xl font-medium pb-16'>커피링크</div>
           <div className="relative">
             <input
@@ -14,11 +14,11 @@ export default function LoginPage() {
               type="email"
               required
               placeholder=" "
-              className="peer w-full rounded-md border border-gray-200 px-3 py-3 focus:border-yellow-800 focus:ring-1 focus:ring-yellow-800 focus:outline-hidden"
+              className="peer w-full rounded-md border border-gray-200 px-3 py-3 focus:border-primary-500 focus:ring-1 focus:ring-primary-500 focus:outline-hidden"
             />
             <label
               htmlFor="id"
-              className="absolute top-3.5 left-3 px-1 text-sm text-gray-400 transition-all peer-not-placeholder-shown:-top-1.5 peer-not-placeholder-shown:z-50 peer-not-placeholder-shown:bg-white peer-not-placeholder-shown:text-xs peer-focus:-top-1.5 peer-focus:bg-white peer-focus:text-xs peer-focus:text-yellow-800"
+              className="absolute top-3.5 left-3 px-1 text-sm rounded-md text-gray-400 transition-all peer-not-placeholder-shown:-top-1.5 peer-not-placeholder-shown:z-50 peer-not-placeholder-shown:bg-white peer-not-placeholder-shown:text-xs peer-focus:-top-1.5 peer-focus:bg-white peer-focus:text-xs peer-focus:text-primary-500"
             >
               이메일
             </label>
@@ -34,11 +34,11 @@ export default function LoginPage() {
               type="password"
               required
               placeholder=" "
-              className="peer w-full rounded-md border border-gray-200 px-3 py-3 focus:border-yellow-800 focus:ring-1 focus:ring-yellow-800 focus:outline-hidden"
+              className="peer w-full rounded-md border border-gray-200 px-3 py-3 focus:border-primary-500 focus:ring-1 focus:ring-primary-500 focus:outline-hidden"
             />
             <label
               htmlFor="password"
-              className="absolute top-3.5 left-3 px-1 text-sm text-gray-400 transition-all peer-not-placeholder-shown:-top-1.5 peer-not-placeholder-shown:z-50 peer-not-placeholder-shown:bg-white peer-not-placeholder-shown:text-xs peer-focus:-top-1.5 peer-focus:bg-white peer-focus:text-xs peer-focus:text-yellow-800"
+              className="absolute top-3.5 left-3 px-1 text-sm text-gray-400 transition-all peer-not-placeholder-shown:-top-1.5 peer-not-placeholder-shown:z-50 peer-not-placeholder-shown:bg-white peer-not-placeholder-shown:text-xs peer-focus:-top-1.5 peer-focus:bg-white peer-focus:text-xs peer-focus:text-primary-500"
             >
               비밀번호
             </label>
@@ -46,7 +46,8 @@ export default function LoginPage() {
           {/* <label htmlFor="password">Password:</label>
         <input id="password" name="password" type="password" required /> */}
           <div>
-            <button className="bg-yellow-700 hover:bg-yellow-900 mt-5 w-full cursor-pointer rounded-md py-2.5 text-white transition-all hover:text-white" formAction={login}>로그인</button>
+            <Button className="bg-primary-500 hover:bg-primary-600 mt-5 w-full cursor-pointer rounded-md py-2.5 text-white transition-all hover:text-white" formAction={login}>로그인</Button>
+            {/* <button className="bg-primary-500 hover:bg-primary-600 mt-5 w-full cursor-pointer rounded-md py-2.5 text-white transition-all hover:text-white" formAction={login}>로그인</button> */}
           </div>
           {/* <div>
             <button className="bg-gray-300 hover:bg-gray-700 mt-5 w-full cursor-pointer rounded-md py-2.5 text-black transition-all hover:text-white" formAction={signup}>회원가입</button>
--- src/components/church-nav.tsx
+++ src/components/church-nav.tsx
@@ -60,7 +60,7 @@ const Nav = ({ user }: { user: IUser }) => {
         <div className="flex container justify-between items-center">
           {!isDesktop && user.type === 'cafe' && <SidebarTrigger />}
           <div>
-            <p className="text-2xl text-primary-500 font-medium tracking-tight">원두 기부</p>
+            <p className="text-2xl font-bold text-primary-500 tracking-tight">원두 기부</p>
           </div>
           <div className='flex items-center gap-5'>
             <Profile user={user} />
